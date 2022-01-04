b6e660ce8d343ee2416cfdca9eb583f16315d343
GigaNByte <karol.saj0@gmail.com>
2022-01-04 21:32:27 +0100

    refactorization of whole project

---
+++ bloodDbExampleQueries-Postgresql.sql
@@ -0,0 +1,11 @@
+// Wybierz wszystkich pacjentów z grupą krwi ab
+Select * from blooddb.patients where patients."bloodTypeId" IN (Select id from blooddb.blood where blood.type='AB');
+
+
+//Wybierz wszystkich pacjentów i dawców z grupa krwi a- i 0+
+SELECT blooddb.donors.id ,blooddb.donors.name, blooddb.donors.surname, blooddb.donors."bloodTypeId" FROM blooddb.donors where donors."bloodTypeId" IN (Select id from blooddb.blood where blood."fullName"='A-' or  blood."fullName"='0+' )
+UNION
+Select blooddb.patients.id ,blooddb.patients.name , blooddb.patients.surname, blooddb.patients."bloodTypeId" from blooddb.patients where patients."bloodTypeId" IN (Select id from blooddb.blood where blood."fullName"='A-' or  blood."fullName"='0+' );
+
+//wybierz wszystkie pacjenów zaczynajcych się na litere a 
+Select * from  blooddb.patients   where blooddb.patients.name like '%a';  
--- readme.md
+++ readme.md
@@ -1,5 +1,22 @@
-## How to import schema:
+# Blood Transfusion Database Example
 
-</hr>
+This codebase constists of MySql and Postgre schemas and example queries of few tables that provides simplified blood donor/patient/hospital management database system for CS (Computer Science) education purpouses.
 
-![](HowToImportSchema.gif)
+
+## How to import schema and create query:
+
+<hr></hr>
+
+
+### MySql Workbench:
+
+
+
+![](./readme/HowToImportSchema-MySqlWorkbench.gif)
+
+
+<hr></hr>
+### PGAdmin v4:
+
+
+![](./readme/HowToImportSchema-Postgre.gif)
