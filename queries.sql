a5d2c8860820b5b801f471bfd4fba53dc5dd3f4b
GigaNByte <karol.saj0@gmail.com>
2022-01-04 15:48:12 +0100

    added second query

--- queries.sql
+++ queries.sql
@@ -1,1 +1,8 @@
+// Wybierz wszystkich pacjentów z grupą krwi ab
 Select * from blooddb.patients where patients."bloodTypeId" IN (Select id from blooddb.blood where blood.type='AB');
+
+
+//Wybierz wszystkich pacjentów i dawców z grupa krwi a- i 0+
+SELECT blooddb.donors.id ,blooddb.donors.name, blooddb.donors.surname, blooddb.donors."bloodTypeId" FROM blooddb.donors where donors."bloodTypeId" IN (Select id from blooddb.blood where blood."fullName"='A-' or  blood."fullName"='0+' )
+UNION
+Select blooddb.patients.id ,blooddb.patients.name , blooddb.patients.surname, blooddb.patients."bloodTypeId" from blooddb.patients where patients."bloodTypeId" IN (Select id from blooddb.blood where blood."fullName"='A-' or  blood."fullName"='0+' );
